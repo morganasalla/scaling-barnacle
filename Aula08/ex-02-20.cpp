4c87eb6003436f224368ddd2194fc4907122315d
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2018-04-04 21:01:42 -0300

    exercicios Deitel (2)

---
+++ Aula08/ex-02-20.cpp
@@ -0,0 +1,36 @@
+/*
+
+Morgana
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+	int n;
+
+	cout << "Informe um numero: ";
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "é par" << endl;
+	} else {
+		cout << "é ímpar" << endl;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
